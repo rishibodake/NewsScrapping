7cb4afea7b0d6473faf8ec12d08f4f9b7edee52f
abhib <rishibodake@gmail.com>
2020-07-17 15:30:19 +0530

    Added:Class For Data Scrapping

---
+++ NewsScrapping/DataScapper.cs
@@ -0,0 +1,14 @@
+﻿
+using OpenQA.Selenium;
+
+namespace NewsScrapping
+{
+    public class DataScapper
+    {
+        IWebDriver driver;
+        public DataScapper(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+    }
+}
