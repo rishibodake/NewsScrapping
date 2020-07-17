a8a0649548b371a5551dbd327d8a10d3aa5ace68
abhib <rishibodake@gmail.com>
2020-07-17 15:06:37 +0530

    Added:Method To Access The Web Url

---
+++ NewsScrapping/AccessWeb.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace NewsScrapping
+{
+    public class AccessWeb
+    {
+        IWebDriver driver = new ChromeDriver();
+        public void OpenLink()
+        {
+            driver.Url = "https://news.ycombinator.com/";
+        }
+    }
+}
